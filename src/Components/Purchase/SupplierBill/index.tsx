ddf160f548dde4147d89669626ab527d9ed9c506
Shakil Gaha <shakilhkdigiverse@gmail.com>
2026-03-14 16:48:26 +0530

    Unused File and code Removed

--- src/Components/Purchase/SupplierBill/index.tsx
+++
@@ -1,8 +0,0 @@
-import SupplierBillDetails from "./SupplierBillDetails";
-import SupplierBillTabs from "./SupplierBillTabs";
-import AdditionalChargesSection from "./AdditionalChargeSection";
-import { TaxDetails } from "./TaxDetails";
-import TermsAndConditionModal from "./TermsAndCondition/TermsAndConditionModal";
-import TermsSelectionModal from "./TermsAndCondition/TermsSelectionModal";
-
-export { SupplierBillDetails, SupplierBillTabs, AdditionalChargesSection, TaxDetails, TermsAndConditionModal, TermsSelectionModal };
--- src/Components/Purchase/index.tsx
+++ src/Components/Purchase/index.tsx
@@ -1,4 +1,6 @@
 import PurchaseOrderTabs from "./PurchaseOrder/PurchaseOrderTabs";
 import PurchaseOrderDetails from "./PurchaseOrder/PurchaseOrderDetails";
+import SupplierBillDetails from "./SupplierBill/SupplierBillDetails";
+import SupplierBillTabs from "./SupplierBill/SupplierBillTabs";
 
-export { PurchaseOrderDetails, PurchaseOrderTabs };
+export { PurchaseOrderDetails, PurchaseOrderTabs, SupplierBillDetails, SupplierBillTabs };
