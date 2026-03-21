fd756c2b1af7ea5cf84dd2ae6a58cc7a1ea4b238
Harshit <harshithkdigiverse@gmail.com>
2026-03-21 17:50:02 +0530

    Update in user page

--- src/Pages/User/index.tsx
+++ src/Pages/User/index.tsx
@@ -2,12 +2,13 @@ import { Box, Grid } from "@mui/material";
 import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { Mutations, Queries } from "../../Api";
-import { AdvancedSearch, CommonActionColumn, CommonBreadcrumbs, CommonCard, CommonDataGrid, CommonDeleteModal, CommonObjectNameColumn, CommonPhoneColumns } from "../../Components/Common";
+import { AdvancedSearch, CommonActionColumn, CommonBreadcrumbs, CommonCard, CommonDataGrid, CommonDeleteModal, CommonObjectNameColumn } from "../../Components/Common";
 import { PAGE_TITLE, ROUTES } from "../../Constants";
 import { BREADCRUMBS } from "../../Data";
 import type { AppGridColDef, UserBase } from "../../Types";
 import { useDataGrid, usePagePermission } from "../../Utils/Hooks";
 import { CreateFilter, GenerateOptions } from "../../Utils";
+import { CommonObjectPropertyColumn } from "../../Components/Common/CommonDataGrid/CommonColumns";
 
 const User = () => {
   const { paginationModel, setPaginationModel, sortModel, setSortModel, filterModel, setFilterModel, rowToDelete, setRowToDelete, isActive, setActive, params, advancedFilter, updateAdvancedFilter } = useDataGrid();
@@ -34,7 +35,7 @@ const User = () => {
     { field: "fullName", headerName: "Full Name", width: 170 },
     { field: "designation", headerName: "Designation", width: 170 },
     { field: "email", headerName: "Email", width: 240 },
-    CommonPhoneColumns<UserBase>("phoneNo", { headerName: "Phone No", width: 150 }),
+    CommonObjectPropertyColumn<UserBase>("phoneNo", "phoneNo", ["countryCode", "phoneNo"], { headerName: "Phone No", width: 150, type: "phone" }),
     { field: "panNumber", headerName: "PAN Number", width: 150 },
     { field: "wages", headerName: "Wages", type: "number", width: 150 },
     { field: "extraWages", headerName: "Extra Wages", type: "number", width: 150 },
@@ -67,6 +68,7 @@ const User = () => {
     onSortModelChange: setSortModel,
     filterModel,
     onFilterModelChange: setFilterModel,
+    fileName:PAGE_TITLE.USER.BASE,
   };
   const filter = [CreateFilter("Select Company", "companyFilter", advancedFilter, updateAdvancedFilter, GenerateOptions(CompanyData?.data), CompanyDataLoading, { xs: 12, sm: 6, md: 3 })];
   return (
